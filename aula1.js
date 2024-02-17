9beebab83c2bfce62d2f34064afc2b2cf2fd71cd
2302763 <2302763@salas.aulas>
2024-02-16 22:08:24 -0300

    enviando código

---
+++ aula1.js
@@ -0,0 +1,14 @@
+endereço = {
+    "cep": "02524-000",
+    "logradouro": "Rua do Aclamado",
+    "complemento": "",
+    "bairro": "Jardim São Bento",
+    "localidade": "São Paulo",
+    "uf": "SP",
+    "ibge": "3550308",
+    "gia": "1004",
+    "ddd": "11",
+    "siafi": "7107"
+  }
+
+  console.log(endereço.cep)
